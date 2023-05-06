f6adb177a177fe393421b17b30323beb4ef428de
Soulninja4 <getarceus@gmail.com>
2023-05-07 00:56:07 +0530

    first commit

---
+++ src/main/java/soul/nihal/occultarts/OccultArtsClient.java
@@ -0,0 +1,11 @@
+package soul.nihal.occultarts;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OccultArtsClient implements ClientModInitializer {
+
+  @Override
+  public void onInitializeClient() {
+  }
+
+}
---
+++ src/main/java/soul/nihal/occultarts/OccultArtsDataGenerator.java
@@ -0,0 +1,11 @@
+package soul.nihal.occultarts;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OccultArtsDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
